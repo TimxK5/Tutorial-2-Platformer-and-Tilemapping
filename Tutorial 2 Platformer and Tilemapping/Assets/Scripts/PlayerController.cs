6591db9dd07828a02ba763b8d97127192fb2dba1
TimxK5 <59641989+TimxK5@users.noreply.github.com>
2020-02-22 08:29:52 -0500

    Tutorial 2 Hiram Sun

---
+++ Tutorial 2 Platformer and Tilemapping/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    public float speed;
+
+    public Text score;
+    private int scoreValue = 0;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        score.text = scoreValue.ToString();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb2d.AddForce(new Vector2(moveHorizontal * speed, moveVertical * speed));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Coin")
+        {
+            scoreValue += 1;
+            score.text = scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+            }
+        }
+    }
+}
